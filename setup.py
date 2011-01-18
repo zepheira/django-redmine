0960664171440cfc8936d5586141b29fe4faadb9
David Feeney <dfeeney@gmail.com>
2011-01-18 16:20:24 -0500

    tag based version numbering

--- setup.py
+++ setup.py
@@ -3,6 +3,37 @@ Setup for the Django Redmine app.
 """
 from setuptools import setup, find_packages
 
+
+VERSION = __import__('django_redmine').__version__
+
+from distutils.command.sdist import sdist
+from distutils.command.build import build
+import os
+
+def write_version_file():
+    version_info = __import__('django_redmine').__version_info__
+    f = open("django_redmine/version.py", "w")
+    f.write('__version_info__ = %s\n' % (version_info,))
+    f.close()
+
+def remove_version_file():
+    os.remove("django_redmine/version.py")
+
+class sdist_version(sdist):
+
+    def run(self):
+        write_version_file()
+        sdist.run(self)
+        remove_version_file()
+
+class build_version(build):
+
+    def run(self):
+        write_version_file()
+        build.run(self)
+        remove_version_file()
+
+
 setup(
     name = "django-redmine",
     version = "1.0.0",
@@ -13,5 +44,6 @@ setup(
     author_email = u"ryanlee@zepheira.com",
     url = "https://github.com/zepheira/django-redmine",
     packages = find_packages(),
-    include_package_data=True
+    include_package_data=True,
+    cmdclass = {'sdist': sdist_version, 'build': build_version}
 )
